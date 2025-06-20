0b12430c0300af36ef7414d0cb55265ffd3551ea
Amarnath Kumar <amarnathkumar1455@gmail.com>
2025-06-21 00:09:56 +0530

    creating express server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log("Let'start!");
+const express = require("express");
+
+const app = express();
+
+app.use("/test", (req, res) => {
+    res.send("routing test..");
+});
+
+app.use((req, res) => {
+    res.send("Hello from the server");
+});
+
+app.listen(1234, () => {
+    console.log("Server is successfully listenong on port 1234...");
+    
+});
