b677360c17b98b54809bfb5342dc3d917b738108
Amarnath Kumar <amarnathkumar1455@gmail.com>
2025-06-21 11:09:55 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,21 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-    res.send({firstname: "Amarnath", lastname: "Kumar"})
-})
 
-app.post("/user", (req, res) => {
-    res.send("Saved data to DB successfully...")
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully...")
-})
-
-app.use("/test", (req, res) => {
-    res.send("Hello from the server");
+app.get("/user/:UserId/:name", (req, res) => {
+    console.log(req.params);
+    res.send({firstname: "Amarnath", lastname: "Kumar"});
 });
+
 
 app.listen(1234, () => {
     console.log("Server is successfully listenong on port 1234...");
