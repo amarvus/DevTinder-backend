2bc0c4a07ecbeb7b037723f213063f42edcee7ee
Amarnath Kumar <amarnathkumar1455@gmail.com>
2025-06-21 10:37:44 +0530

    request handlers

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ const express = require("express");
 
 const app = express();
 
+// routing sequence matters
+
 app.use("/test", (req, res) => {
     res.send("routing test..");
 });
 
-app.use((req, res) => {
+app.use("/hello", (req, res) => {
+    res.send("hello hello hello..!");
+});
+
+app.use("/", (req, res) => {
     res.send("Hello from the server");
 });
 
