8189f4336d5b18d08f10894a52a2a9dd69baa16a
Amarnath Kumar <amarnathkumar1455@gmail.com>
2025-06-21 20:50:21 +0530

    multiple route-handlers

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const app = express();
 
-
-app.get("/user/:UserId/:name", (req, res) => {
-    console.log(req.params);
-    res.send({firstname: "Amarnath", lastname: "Kumar"});
-});
-
+app.use("/user", (req, res, next) => {
+    //res.send("Route handlers...1");
+    next();
+},
+(req, res) => {
+    res.send("Route handlers...2");
+}
+);
 
 app.listen(1234, () => {
-    console.log("Server is successfully listenong on port 1234...");
+    console.log("Server is successfully listening on port 1234...");
     
 });
